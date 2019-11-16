dcd56246fabec3e3e70aaeb68c37039e3eede86d
Enoch-Khanh <khanhparkour12345@gmail.com>
2019-11-16 20:20:15 +0700

    dang lam trang login

--- js/login.js
+++ js/login.js
@@ -1,8 +1,3 @@
 $(document).ready(function() {
-  $("#facebook-login").on("click", () => {
-    FB.getLoginStatus(function(response) {
-      statusChangeCallback(response);
-      console.log(response);
-    });
-  });
+  $("#facebook-login").on("click", () => {});
 });
