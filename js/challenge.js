3241099938ade53a7d5f935a2f2320a5636249ad
Enoch-Khanh <khanhparkour12345@gmail.com>
2019-11-18 14:27:15 +0700

    alo

--- js/challenge.js
+++ js/challenge.js
@@ -65,4 +65,131 @@ $(document).ready(function() {
       $("#extreme").attr("checked", false);
     }
   });
+
+  const value = {
+    exercises: [
+      {
+        id: 1,
+        name: "Say hello word!",
+        level: "Dễ",
+        level_code: 1,
+        url: "detail.html"
+      },
+      {
+        id: 2,
+        name: "Say hello word!",
+        level: "Khó",
+        level_code: 3,
+        url: "detail.html"
+      },
+      {
+        id: 3,
+        name: "Say hello word!",
+        level: "Bình Thường",
+        level_code: 2,
+        url: "detail.html"
+      },
+      {
+        id: 4,
+        name: "Say hello word!",
+        level: "Cực Khó",
+        level_code: 4,
+        url: "detail.html"
+      },
+      {
+        id: 5,
+        name: "Say hello word!",
+        level: "Dễ",
+        level_code: 1,
+        url: "detail.html"
+      },
+      {
+        id: 6,
+        name: "Say hello word!",
+        level: "Khó",
+        level_code: 3,
+        url: "detail.html"
+      },
+      {
+        id: 7,
+        name: "Say hello word!",
+        level: "Bình Thường",
+        level_code: 2,
+        url: "detail.html"
+      },
+      {
+        id: 8,
+        name: "Say hello word!",
+        level: "Cực Khó",
+        level_code: 4,
+        url: "detail.html"
+      }
+    ]
+  };
+
+  const renderExercises = () => {
+    let exercisesObj = value.exercises,
+      x;
+    for (x in exercisesObj) {
+      if (exercisesObj[x].level_code == 1) {
+        $("#exercises-challenge").append(`<div class="card">
+        <div class="card-content w-50">
+          <h4 class="card-title grey-text text-darken-4">
+            ${exercisesObj[x].name}
+          </h4>
+          <p class="cyan-text darken-4">${exercisesObj[x].level}</p>
+        </div>
+        <a
+          class="btn btn-large waves-effect waves-light btn-challenge"
+          href="${exercisesObj[x].url}"
+          >hit challenge</a
+        >
+      </div>`);
+      } else if (exercisesObj[x].level_code == 2) {
+        $("#exercises-challenge").append(`<div class="card">
+        <div class="card-content w-50">
+          <h4 class="card-title grey-text text-darken-4">
+            ${exercisesObj[x].name}
+          </h4>
+          <p class="green-text darken-4">${exercisesObj[x].level}</p>
+        </div>
+        <a
+          class="btn btn-large waves-effect waves-light btn-challenge"
+          href="${exercisesObj[x].url}"
+          >hit challenge</a
+        >
+      </div>`);
+      } else if (exercisesObj[x].level_code == 3) {
+        $("#exercises-challenge").append(`<div class="card">
+        <div class="card-content w-50">
+          <h4 class="card-title grey-text text-darken-4">
+            ${exercisesObj[x].name}
+          </h4>
+          <p class="orange-text darken-4">${exercisesObj[x].level}</p>
+        </div>
+        <a
+          class="btn btn-large waves-effect waves-light btn-challenge"
+          href="${exercisesObj[x].url}"
+          >hit challenge</a
+        >
+      </div>`);
+      } else if (exercisesObj[x].level_code == 4) {
+        $("#exercises-challenge").append(`<div class="card">
+        <div class="card-content w-50">
+          <h4 class="card-title grey-text text-darken-4">
+            ${exercisesObj[x].name}
+          </h4>
+          <p class="red-text darken-4">${exercisesObj[x].level}</p>
+        </div>
+        <a
+          class="btn btn-large waves-effect waves-light btn-challenge"
+          href="${exercisesObj[x].url}"
+          >hit challenge</a
+        >
+      </div>`);
+      }
+    }
+  };
+
+  renderExercises();
 });
