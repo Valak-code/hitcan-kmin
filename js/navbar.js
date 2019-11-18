3ae06c6080e3da40d9ec31a14b4c724149963eea
Enoch-Khanh <khanhparkour12345@gmail.com>
2019-11-18 21:44:53 +0700

    alo

--- js/navbar.js
+++ js/navbar.js
@@ -11,4 +11,18 @@ $(document).ready(function() {
     }
     prevScrollpos = currentScrollPos;
   });
+  console.log(
+    "%cPhi Khanh đẹp trai vô đối",
+    "color: #1AB374; font-weight: bold; font-size: 3rem; text-shadow: 0 0 5px rgba(0, 0, 0, 0.2);"
+  );
+
+  console.log(
+    "%cvô đây kím info bạn Khanh đẹp trai phải hong :)))",
+    "color: #B055FA; font-weight: bold; font-size: 1.7rem; text-shadow: 0 0 5px rgba(0, 0, 0, 0.2);"
+  );
+
+  console.log(
+    "%cKhông cho đâu Ahihi :))))",
+    "color: #ff0000; font-weight: bold; font-size: 1.3rem; text-shadow: 0 0 5px rgba(0, 0, 0, 0.2);"
+  );
 });
