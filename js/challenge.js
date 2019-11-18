0502167409d16344a4060fc26837e9566e286442
Enoch-Khanh <khanhparkour12345@gmail.com>
2019-11-18 15:40:08 +0700

    em update xong phần filter rồi nha anh :))))) anh làm thử phần login đi

--- js/challenge.js
+++ js/challenge.js
@@ -1,72 +1,152 @@
-$(document).ready(function() {
-  $(".modal").modal();
-
-  $("#easy").on("change", () => {
-    if ($("#easy").is(":checked")) {
-      $("#easy-mobile").attr("checked", true);
-    } else {
-      $("#easy-mobile").attr("checked", false);
-      console.log($("#easy-mobile").is(":checked"));
+const renderExercisesEasy = exercisesObj => {
+  let x;
+  for (x in exercisesObj) {
+    if (exercisesObj[x].level_code == 1) {
+      $("#exercises-challenge").append(`<div class="card easy">
+      <div class="card-content w-50">
+        <h4 class="card-title grey-text text-darken-4">
+          ${exercisesObj[x].name}
+        </h4>
+        <p class="cyan-text darken-4">${exercisesObj[x].level}</p>
+      </div>
+      <a
+        class="btn btn-large waves-effect waves-light btn-challenge"
+        href="${exercisesObj[x].url}"
+        >hit challenge</a
+      >
+    </div>`);
     }
-  });
+  }
+};
 
-  $("#easy-mobile").on("change", () => {
-    if ($("#easy-mobile").is(":checked")) {
-      $("#easy").attr("checked", true);
-    } else {
-      $("#easy").attr("checked", false);
+const renderExercisesNormal = exercisesObj => {
+  let x;
+  for (x in exercisesObj) {
+    if (exercisesObj[x].level_code == 2) {
+      $("#exercises-challenge").append(`<div class="card normal">
+      <div class="card-content w-50">
+        <h4 class="card-title grey-text text-darken-4">
+          ${exercisesObj[x].name}
+        </h4>
+        <p class="green-text darken-4">${exercisesObj[x].level}</p>
+      </div>
+      <a
+        class="btn btn-large waves-effect waves-light btn-challenge"
+        href="${exercisesObj[x].url}"
+        >hit challenge</a
+      >
+    </div>`);
     }
-  });
+  }
+};
 
-  $("#normal").on("change", () => {
-    if ($("#normal").is(":checked")) {
-      $("#normal-mobile").attr("checked", true);
-    } else {
-      $("#normal-mobile").attr("checked", false);
+const renderExercisesHard = exercisesObj => {
+  let x;
+  for (x in exercisesObj) {
+    if (exercisesObj[x].level_code == 3) {
+      $("#exercises-challenge").append(`<div class="card hard">
+      <div class="card-content w-50">
+        <h4 class="card-title grey-text text-darken-4">
+          ${exercisesObj[x].name}
+        </h4>
+        <p class="orange-text darken-4">${exercisesObj[x].level}</p>
+      </div>
+      <a
+        class="btn btn-large waves-effect waves-light btn-challenge"
+        href="${exercisesObj[x].url}"
+        >hit challenge</a
+      >
+    </div>`);
     }
-  });
+  }
+};
 
-  $("#normal-mobile").on("change", () => {
-    if ($("#normal-mobile").is(":checked")) {
-      $("#normal").attr("checked", true);
-    } else {
-      $("#normal").attr("checked", false);
+const renderExercisesExtreme = exercisesObj => {
+  let x;
+  for (x in exercisesObj) {
+    if (exercisesObj[x].level_code == 4) {
+      $("#exercises-challenge").append(`<div class="card extreme">
+      <div class="card-content w-50">
+        <h4 class="card-title grey-text text-darken-4">
+          ${exercisesObj[x].name}
+        </h4>
+        <p class="red-text darken-4">${exercisesObj[x].level}</p>
+      </div>
+      <a
+        class="btn btn-large waves-effect waves-light btn-challenge"
+        href="${exercisesObj[x].url}"
+        >hit challenge</a
+      >
+    </div>`);
     }
-  });
+  }
+};
 
-  $("#hard").on("change", () => {
-    if ($("#hard").is(":checked")) {
-      $("#hard-mobile").attr("checked", true);
-    } else {
-      $("#hard-mobile").attr("checked", false);
+const renderExercisesAll = exercisesObj => {
+  let x;
+  for (x in exercisesObj) {
+    if (exercisesObj[x].level_code == 1) {
+      $("#exercises-challenge").append(`<div class="card easy">
+      <div class="card-content w-50">
+        <h4 class="card-title grey-text text-darken-4">
+          ${exercisesObj[x].name}
+        </h4>
+        <p class="cyan-text darken-4">${exercisesObj[x].level}</p>
+      </div>
+      <a
+        class="btn btn-large waves-effect waves-light btn-challenge"
+        href="${exercisesObj[x].url}"
+        >hit challenge</a
+      >
+    </div>`);
+    } else if (exercisesObj[x].level_code == 2) {
+      $("#exercises-challenge").append(`<div class="card normal">
+      <div class="card-content w-50">
+        <h4 class="card-title grey-text text-darken-4">
+          ${exercisesObj[x].name}
+        </h4>
+        <p class="green-text darken-4">${exercisesObj[x].level}</p>
+      </div>
+      <a
+        class="btn btn-large waves-effect waves-light btn-challenge"
+        href="${exercisesObj[x].url}"
+        >hit challenge</a
+      >
+    </div>`);
+    } else if (exercisesObj[x].level_code == 3) {
+      $("#exercises-challenge").append(`<div class="card hard">
+      <div class="card-content w-50">
+        <h4 class="card-title grey-text text-darken-4">
+          ${exercisesObj[x].name}
+        </h4>
+        <p class="orange-text darken-4">${exercisesObj[x].level}</p>
+      </div>
+      <a
+        class="btn btn-large waves-effect waves-light btn-challenge"
+        href="${exercisesObj[x].url}"
+        >hit challenge</a
+      >
+    </div>`);
+    } else if (exercisesObj[x].level_code == 4) {
+      $("#exercises-challenge").append(`<div class="card extreme">
+      <div class="card-content w-50">
+        <h4 class="card-title grey-text text-darken-4">
+          ${exercisesObj[x].name}
+        </h4>
+        <p class="red-text darken-4">${exercisesObj[x].level}</p>
+      </div>
+      <a
+        class="btn btn-large waves-effect waves-light btn-challenge"
+        href="${exercisesObj[x].url}"
+        >hit challenge</a
+      >
+    </div>`);
     }
-  });
+  }
+};
 
-  $("#hard-mobile").on("change", () => {
-    if ($("#hard-mobile").is(":checked")) {
-      $("#hard").attr("checked", true);
-    } else {
-      $("#hard").attr("checked", false);
-    }
-  });
-
-  $("#extreme").on("change", () => {
-    if ($("#extreme").is(":checked")) {
-      $("#extreme-mobile").attr("checked", true);
-    } else {
-      $("#extreme-mobile").attr("checked", false);
-    }
-  });
-
-  $("#extreme-mobile").on("change", () => {
-    if ($("#extreme-mobile").is(":checked")) {
-      $("#extreme").attr("checked", true);
-    } else {
-      $("#extreme").attr("checked", false);
-    }
-  });
-
-  // em sẽ nhận được 1 file json như này;
+$(document).ready(() => {
+  // gọi API em sẽ nhận được 1 file json như này;
   const value = {
     exercises: [
       {
@@ -128,70 +208,152 @@ $(document).ready(function() {
     ]
   };
 
-  //sau đó render ra thành html
-  const renderExercises = () => {
-    let exercisesObj = value.exercises,
-      x;
-    for (x in exercisesObj) {
-      if (exercisesObj[x].level_code == 1) {
-        $("#exercises-challenge").append(`<div class="card">
-        <div class="card-content w-50">
-          <h4 class="card-title grey-text text-darken-4">
-            ${exercisesObj[x].name}
-          </h4>
-          <p class="cyan-text darken-4">${exercisesObj[x].level}</p>
-        </div>
-        <a
-          class="btn btn-large waves-effect waves-light btn-challenge"
-          href="${exercisesObj[x].url}"
-          >hit challenge</a
-        >
-      </div>`);
-      } else if (exercisesObj[x].level_code == 2) {
-        $("#exercises-challenge").append(`<div class="card">
-        <div class="card-content w-50">
-          <h4 class="card-title grey-text text-darken-4">
-            ${exercisesObj[x].name}
-          </h4>
-          <p class="green-text darken-4">${exercisesObj[x].level}</p>
-        </div>
-        <a
-          class="btn btn-large waves-effect waves-light btn-challenge"
-          href="${exercisesObj[x].url}"
-          >hit challenge</a
-        >
-      </div>`);
-      } else if (exercisesObj[x].level_code == 3) {
-        $("#exercises-challenge").append(`<div class="card">
-        <div class="card-content w-50">
-          <h4 class="card-title grey-text text-darken-4">
-            ${exercisesObj[x].name}
-          </h4>
-          <p class="orange-text darken-4">${exercisesObj[x].level}</p>
-        </div>
-        <a
-          class="btn btn-large waves-effect waves-light btn-challenge"
-          href="${exercisesObj[x].url}"
-          >hit challenge</a
-        >
-      </div>`);
-      } else if (exercisesObj[x].level_code == 4) {
-        $("#exercises-challenge").append(`<div class="card">
-        <div class="card-content w-50">
-          <h4 class="card-title grey-text text-darken-4">
-            ${exercisesObj[x].name}
-          </h4>
-          <p class="red-text darken-4">${exercisesObj[x].level}</p>
-        </div>
-        <a
-          class="btn btn-large waves-effect waves-light btn-challenge"
-          href="${exercisesObj[x].url}"
-          >hit challenge</a
-        >
-      </div>`);
-      }
-    }
-  };
+  $(".modal").modal();
 
-  renderExercises();
+  renderExercisesAll(value.exercises);
+  let count = 0;
+
+  $("#easy").on("change", () => {
+    if ($("#easy").is(":checked")) {
+      $("#easy-mobile").attr("checked", true);
+      if (count == 0) {
+        $("#exercises-challenge").empty();
+      }
+      renderExercisesEasy(value.exercises);
+      count++;
+    } else {
+      $("#easy-mobile").attr("checked", false);
+      $(".easy").remove();
+      count--;
+    }
+    if (count == 0) {
+      renderExercisesAll(value.exercises);
+    }
+  });
+
+  $("#easy-mobile").on("change", () => {
+    if ($("#easy-mobile").is(":checked")) {
+      $("#easy").attr("checked", true);
+      if (count == 0) {
+        $("#exercises-challenge").empty();
+      }
+      renderExercisesEasy(value.exercises);
+      count++;
+    } else {
+      $("#easy").attr("checked", false);
+      $(".easy").remove();
+      count--;
+    }
+    if (count == 0) {
+      renderExercisesAll(value.exercises);
+    }
+  });
+
+  $("#normal").on("change", () => {
+    if ($("#normal").is(":checked")) {
+      $("#normal-mobile").attr("checked", true);
+      if (count == 0) {
+        $("#exercises-challenge").empty();
+      }
+      renderExercisesNormal(value.exercises);
+      count++;
+    } else {
+      $("#normal-mobile").attr("checked", false);
+      $(".normal").remove();
+      count--;
+    }
+    if (count == 0) {
+      renderExercisesAll(value.exercises);
+    }
+  });
+
+  $("#normal-mobile").on("change", () => {
+    if ($("#normal-mobile").is(":checked")) {
+      $("#normal").attr("checked", true);
+      if (count == 0) {
+        $("#exercises-challenge").empty();
+      }
+      renderExercisesNormal(value.exercises);
+      count++;
+    } else {
+      $("#normal").attr("checked", false);
+      $(".normal").remove();
+      count--;
+    }
+    if (count == 0) {
+      renderExercisesAll(value.exercises);
+    }
+  });
+
+  $("#hard").on("change", () => {
+    if ($("#hard").is(":checked")) {
+      $("#hard-mobile").attr("checked", true);
+      if (count == 0) {
+        $("#exercises-challenge").empty();
+      }
+      renderExercisesHard(value.exercises);
+      count++;
+    } else {
+      $("#hard-mobile").attr("checked", false);
+      $(".hard").remove();
+      count--;
+    }
+    if (count == 0) {
+      renderExercisesAll(value.exercises);
+    }
+  });
+
+  $("#hard-mobile").on("change", () => {
+    if ($("#hard-mobile").is(":checked")) {
+      $("#hard").attr("checked", true);
+      if (count == 0) {
+        $("#exercises-challenge").empty();
+      }
+      renderExercisesHard(value.exercises);
+      count++;
+    } else {
+      $("#hard").attr("checked", false);
+      $(".hard").remove();
+      count--;
+    }
+    if (count == 0) {
+      renderExercisesAll(value.exercises);
+    }
+  });
+
+  $("#extreme").on("change", () => {
+    if ($("#extreme").is(":checked")) {
+      $("#extreme-mobile").attr("checked", true);
+      if (count == 0) {
+        $("#exercises-challenge").empty();
+      }
+      renderExercisesExtreme(value.exercises);
+      count++;
+    } else {
+      $("#extreme-mobile").attr("checked", false);
+      $(".extreme").remove();
+      count--;
+    }
+    if (count == 0) {
+      renderExercisesAll(value.exercises);
+    }
+  });
+
+  $("#extreme-mobile").on("change", () => {
+    if ($("#extreme-mobile").is(":checked")) {
+      $("#extreme").attr("checked", true);
+      if (count == 0) {
+        $("#exercises-challenge").empty();
+      }
+      renderExercisesExtreme(value.exercises);
+      count++;
+    } else {
+      $("#extreme").attr("checked", false);
+      $(".extreme").remove();
+      count--;
+    }
+    if (count == 0) {
+      renderExercisesAll(value.exercises);
+    }
+  });
 });
