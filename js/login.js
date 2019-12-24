c9d6f46f716b2ac66af7176f2e30907ea6f9c0d0
Valak-code <khanhparkour12345@gmail.com>
2019-12-24 14:17:39 +0700

    add new SEO

--- js/login.js
+++ js/login.js
@@ -1,11 +1,4 @@
 $(document).ready(function() {
-  $("#email").on("keyup change", () => {
-    $("#email-pass").attr("src", "img/email.png");
-  });
-  $("#password").on("keyup change", () => {
-    $("#email-pass").attr("src", "img/pass.jpg");
-  });
-
   //login by google
   function onSignIn(googleUser) {
     let profile = googleUser.getBasicProfile();
